7197d4ce97e5315612dfdb44422a04d2f0507d6a
Tilman <tilman.bregler@red-gate.com>
2014-05-28 15:01:43 +0100

    stuff

--- play.clj
+++ play.clj
@@ -10,10 +10,12 @@ foo
   Object
     (toString [this] (apply str (interpose ", " (sort els))))
   clojure.lang.ISeq
-    (next [this] (my-coll. (rest els)))
-    (first [this] (first els))
-    (more [this] (my-coll. (rest els)))
+  (first [this] (first els))
+  (next [this] (my-coll. (rest els)))
+  (more [this] (my-coll. (rest els)))
   )
+
+(defmethod print-method my-coll [v w] (.write w "ajaja"))
 
 (.toString a)
 
@@ -21,19 +23,35 @@ foo
 
 (second a)
 
-(
-(fn [& args]
-(defrecord my-coll [els]
-  Object
-    (toString [this] (apply str (interpose ", " (sort els))))
-  clojure.lang.ISeq
-    (next [this] (my-coll. (rest els)))
-    (first [this] (first els))
-    (more [this] (my-coll. (rest els)))
-	)
-  (my-coll. args)
-)
-1 2 3)
-
 (do (def a (my-coll. [1 2 3])) nil)
+
+(defn make-one [& els]
+  (reify
+  Object
+  (toString [this] (apply str (interpose ", " (sort els))))
+  clojure.lang.ISeq
+  (first [this] (first els))
+  (more [this] (make-one (rest els)))
+  (next [this] (when (> (count els) 1)
+                 make-one (rest els)))
+  clojure.lang.Seqable
+  (seq [this] (when (> (count els) 0)
+                (apply make-one (reduce #(if (some #{%2} %1) %1 (conj %1 %2)) [] els))))
+    clojure.lang.Sequential
+  ))
+
+(let [a (make-one '(1 2 3 4))]
+  (. a (toString)))
+
+(= '(2 1 3) (make-one 2 1 3))
+
+(make-one 2 1 3)
+
+(make-one 2 1 3 3 1 2)
+
+(make-one)
+
+(seq (make-one))
+(str (make-one))
+
 
