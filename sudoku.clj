727d2ea7dec648b6be6f44bd46811e3ac3771376
clivetong <clive.tong@red-gate.com>
2014-07-16 16:39:54 +0100

    Tidy the let binding in the for expression (as per Tilman's suggestion)

--- sudoku.clj
+++ sudoku.clj
@@ -256,11 +256,9 @@
     [board]
     (let [coords (find-empty-point board)]
       (for [possible-item (valid-values-for board coords)
-            solution
-            (let [next-board (set-value-at board coords possible-item)]
-              (solve next-board))]
-        solution
-        ))))
+            :let [next-board (set-value-at board coords possible-item)]
+            solution (solve next-board)]
+        solution))))
 
 
 
@@ -286,15 +284,15 @@
 
 
 (def not-not-solved-board
-  (board [[5 3 0 0 0 8 9 1 2]
-          [6 7 2 1 9 5 3 4 8]
-          [1 9 8 3 4 2 5 6 7]
+  (board [[5 3 0 0 0 0 9 1 2]
+          [0 7 0 1 9 5 3 4 8]
+          [1 9 8 3 4 2 5 0 7]
           [8 5 9 7 6 1 4 2 3]
-          [4 2 6 8 5 3 7 9 1]
+          [4 2 6 8 0 0 0 9 1]
           [7 1 3 9 2 4 8 5 6]
-          [9 6 1 5 3 7 2 8 4]
+          [0 6 1 0 3 7 2 8 4]
           [2 8 7 4 1 9 6 3 5]
-          [3 4 5 2 8 6 1 7 9]]))
+          [3 0 5 2 8 6 1 7 9]]))
 
 (first (solve not-not-solved-board))
 
