5780fa3d27ddd92a61c3b5e4badec0397eb731ce
SimonHolness <simon.holness@red-gate.com>
2014-08-06 13:13:20 +0100

    Extract init and shutdown functions
    Also a 'send' function

--- rabbiting/src/rabbiting/core.clj
+++ rabbiting/src/rabbiting/core.clj
@@ -11,6 +11,8 @@
 (def ^{:const true}
   all-incoming-exchange "all.incoming")
 
+(def ^{:const true} routing-key "rabbiting.message.sent")
+
 (def connected-users (atom #{}))
 
 (defn message-handler
@@ -21,23 +23,56 @@
     (println (format "[consumer] Received a message: %s, delivery tag: %d, content type: %s, type: %s"
                       message delivery-tag content-type type))))
 
-(defn -main
-  [& args]
-  (let [user "arun"
-        conn  (rmq/connect)
+(defn shutdown [{:keys [connection channel]}]
+  (println "[main] Disconnecting...")
+  (rmq/close channel)
+  (rmq/close connection))
+
+(defn queue-name-for-user [user]
+  (str "rabbiting.user." user))
+
+(defn initialize-connection
+  [user]
+  (let [conn  (rmq/connect)
         ch    (lch/open conn)
-        qname (str "rabbiting.user." user)
-        routing-key "rabbiting.message.sent"
+        qname    (queue-name-for-user user)
         ]
+
     (println (format "[main] Connected. Channel id: %d" (.getChannelNumber ch)))
     (lq/declare ch qname :exclusive false :auto-delete true)
     (lq/bind ch qname all-incoming-exchange :routing-key routing-key)
-    (lc/subscribe ch qname message-handler :auto-ack true)
-    (lb/publish ch all-incoming-exchange routing-key user :content-type "text/plain" :type "user.joined")
-    (println "[main] Disconnecting...")
-    (rmq/close ch)
-    (rmq/close conn)))
+    (lc/subscribe ch qname message-handler :auto-ack true )
 
-(-main)
+    { :connection conn :channel ch :user user}
+))
+
+(defn send [msg-type msg-body {:keys [channel user]}]
+    (lb/publish channel
+                all-incoming-exchange
+                routing-key
+                msg-body
+                :content-type "text/plain"
+                :type msg-type
+                :sender user))
+
+(def join (partial send "user.joined"))
+
+(defn -main
+  [& args]
+  (initialize-connection "arun"))
+
+(def connection-info
+  (initialize-connection "arun"))
+
+(def connection-info2
+  (initialize-connection "clivester"))
+
+(join "arun" connection-info)
+
+(join "clivester" connection-info2)
 
 @connected-users
+
+(shutdown connection-info)
+
+(shutdown connection-info2)
