ac17118321193b703aa2b629dccf19edc794d6f0
Tilman <tilman.bregler@red-gate.com>
2014-09-24 13:26:58 +0100

    make number go up...

--- rabbiting/src/cljs/core.cljs
+++ rabbiting/src/cljs/core.cljs
@@ -1,10 +1,19 @@
 (ns rabbiting.core
   (:require [reagent.core :as reagent :refer [atom]]))
 
+(def click-count (atom 0))
+
 (defn msg-list []
   [:div
-   [:h1 "Hello Clojure Lunch"]])
+   [:h1 @click-count]])
+
+(defn asdf [event]
+  (when (= (.-keyCode event) 13)
+    (swap! click-count inc)))
+
+(defn msg-prompt []
+  [:input {:type "text" :on-key-up asdf}])
 
 (defn ^:export run []
-  (reagent/render-component [msg-list]
+  (reagent/render-component [:div [msg-list] [msg-prompt]]
                             (.-body js/document))) 
