695d025d69cec5e176114a1c82a036e521a02ed6
Arun Kumar <arun.kumar@red-gate.com>
2014-10-29 13:09:52 +0000

    Send and receive messages via bus

--- rabbiting/src/clj/rabbiting/core.clj
+++ rabbiting/src/clj/rabbiting/core.clj
@@ -16,12 +16,12 @@
 (def connected-users (atom #{}))
 
 (defn message-handler
-  [ch {:keys [content-type delivery-tag type] :as meta} ^bytes payload]
+  [client-msg-handler ch {:keys [content-type delivery-tag type sender headers] :as meta} ^bytes payload]
   (let [message (String. payload "UTF-8")]
     (when (= type "user.joined")
       (swap! connected-users #(conj % message)))
-    (println (format "[consumer] Received a message: %s, delivery tag: %d, content type: %s, type: %s"
-                      message delivery-tag content-type type))))
+    (println meta)
+    (client-msg-handler (get headers "sender") message)))
 
 (defn shutdown [{:keys [connection channel]}]
   (println "[main] Disconnecting...")
@@ -32,14 +32,14 @@
   (str "rabbiting.user." user))
 
 (defn initialize-connection
-  [user]
+  [client-msg-handler user]
   (let [conn  (rmq/connect)
         ch    (lch/open conn)
         qname    (queue-name-for-user user)]
     (println (format "[main] Connected. Channel id: %d" (.getChannelNumber ch)))
     (lq/declare ch qname :exclusive false :auto-delete true)
     (lq/bind ch qname all-incoming-exchange :routing-key routing-key)
-    (lc/subscribe ch qname message-handler :auto-ack true )
+    (lc/subscribe ch qname (partial message-handler client-msg-handler) :auto-ack true )
     {:connection conn :channel ch :user user}))
 
 (defn send [msg-type msg-body {:keys [channel user]}]
@@ -49,7 +49,10 @@
                 msg-body
                 :content-type "text/plain"
                 :type msg-type
-                :sender user))
+                :headers {"sender" user}))
 
 (defn join [connection-info]
   (send "user.joined" (:user connection-info) connection-info))
+
+(defn message! [body connection-info]
+  (send "user.msg.sent" body connection-info))
--- rabbiting/src/clj/rabbiting/server.clj
+++ rabbiting/src/clj/rabbiting/server.clj
@@ -6,7 +6,11 @@
 
 (def clients (atom {}))
 
-(defn push-chat-message [con args] (send! con (str "Roundtripped " args)))
+(defn push-chat-message [chat-con args]
+  (rc/message! (first args)  chat-con))
+
+(defn recv-chat-message [con user message]
+  (send! con (str user ": " message)))
 
 (defn websocket
   [req]
@@ -14,13 +18,14 @@
   (with-channel req con
 		(swap! clients assoc con true)
 		(println con " connected")
-		
-		(let [chat-con (rc/initialize-connection "Toby")]
-		
+
+		(let [chat-con (rc/initialize-connection (partial recv-chat-message con) "Toby")]
+
 		     (on-receive con (fn [& args]
 					 (println args)
-					 (push-chat-message con args)))
+					 (push-chat-message chat-con args)))
 		     (on-close con (fn [status]
+               (rc/shutdown chat-con)
 				       (swap! clients dissoc con)
 				       (println con " disconnected. status: " status))))))
 
