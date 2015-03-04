c454f8bf17484cc537496c6222a22d69482def90
Tilman <tilman.bregler@red-gate.com>
2015-03-04 13:17:21 +0000

    random hacking

--- plankton/src/plankton/core.clj
+++ plankton/src/plankton/core.clj
@@ -3,13 +3,32 @@
 
 (use 'mikera.image.core)
 (use 'mikera.image.colours)
+(use 'mikera.image.filters)
 
 (def image-path "test-image.jpg")
 
 (def plankton-img (load-image image-path))
 
+(def pixels (get-pixels plankton-img))
+
+(defn threshold-pixels [pixels]
+  (let [av (/ (reduce + 0 pixels) (count pixels))]
+    (map
+     #(if (> %  av)
+        127
+        0)
+     pixels)
+    ))
+
+(defn pixelfilter [f image] (let [pixels (get-pixels image)]
+                              (set-pixels image (f pixels)))
+  )
+
+(defn threshold [image] (pixelfilter threshold-pixels image))
+
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (show plankton-img))
-
+  (threshold plankton-img)
+  (show plankton-img :zoom 5.0))
