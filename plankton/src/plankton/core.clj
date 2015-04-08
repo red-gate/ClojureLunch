bd14f97f48cb1f1aecb6920778df1624f0c37212
Tilman <tilman.bregler@red-gate.com>
2015-04-08 13:03:41 +0100

    broken flood fill

--- plankton/src/plankton/core.clj
+++ plankton/src/plankton/core.clj
@@ -39,23 +39,26 @@
   (pixelfilter threshold-pixels image)
   image)
 
+
 (defn flood-fill-from-index-with-colour [pixels index new-colour]
   (loop [pixels-to-check (list index)]
     (when (first pixels-to-check)
     (let [current (first pixels-to-check)]
-      (when (not= (aget pixels current) new-colour)
+      (when (not= (get pixels current) new-colour)
         (aset pixels current new-colour)
         ;; add neighbouhs
         )
       (recur (rest pixels-to-check)))))
   pixels)
 
+(defn flood-fill-pixels [pixels]
+  (let [enumerated-pixels (zipmap pixels (iterate inc 0))
+        index (find enumerated-pixels 255)]
+       (flood-fill-from-index-with-colour pixels index 100))
+  )
+
 (defn flood-fill-image [image]
-   (let [pixels (byte-array (map byte-mangling (seq (get-pixels image))))
-         enumerated-pixels (zipmap pixels (iterate inc 0))]
-     (let [index (find enumerated-pixels 255)]
-       (set-pixels image (flood-fill-from-index-with-colour pixels index 100))
-       image)))
+   (pixelfilter flood-fill-pixels image))
 
 (deftype Filter [^BufferedImageOp image-op]
   clojure.lang.IFn
