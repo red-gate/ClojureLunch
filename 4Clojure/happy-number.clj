907fba32326c6438aac5a17be7698cb8fec07afc
clivetong <clive.tong@red-gate.com>
2015-10-27 11:28:03 +0000

    I seem to have more work ont his example than the main repository

--- 4Clojure/happy-number.clj
+++ 4Clojure/happy-number.clj
@@ -21,7 +21,7 @@
 	     true
 	   (happy-set y (conj s x))))))
 
-(defn happy-number [x] 
+(defn happy-number [x]
   (happy-set x #{}))
 
 (digits 52345)
@@ -36,6 +36,44 @@
 (happy-number 10000)
 (happy-number 1)
 
+(def happy-number
+(letfn [( digits [x]
+  (if (= x 0)
+      nil
+    (conj
+     (digits
+      (quot x 10))
+     (rem x 10))))
+
+( step-number[x]
+  (apply +
+   (map
+    (fn [i] (* i i))
+    (digits x))))
 
 
-// We should try the infinite stream idea using iterate, take-while 
+(happy-set [x s]
+  (if (s x)
+      false
+    (let [y (step-number x)]
+	 (if (= 1 x)
+	     true
+	   (happy-set y (conj s x))))))
+
+( happy-number [x]
+  (happy-set x #{}))]
+
+
+  happy-number)
+)
+
+(happy-number 7)
+(happy-number 3)
+(happy-number 986543210)
+(happy-number 2)
+(happy-number 10000)
+(happy-number 1)
+
+
+
+
