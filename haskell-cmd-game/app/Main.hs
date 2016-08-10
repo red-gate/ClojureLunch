fa8c02bfca4f40c7c051843f3b94cbe241f7d71b
Toby Smyth <toby.smyth@red-gate.com>
2016-08-10 13:10:05 +0100

    Moving circle

--- haskell-cmd-game/app/Main.hs
+++ haskell-cmd-game/app/Main.hs
@@ -3,20 +3,29 @@ module Main where
 import Lib
 import Control.Monad.State.Strict
 import System.IO
+import Graphics.Gloss
+
+
+data World = MyWorld { timeUntilMove  :: Float, position :: (Int, Int)  }
+ deriving Show
+
+moveInterval :: Float
+moveInterval = 1
+
+initialWorld = MyWorld { timeUntilMove = moveInterval, position = (20,20) }
+
+step :: Float -> World -> World
+step delta (MyWorld timeUntilMove (x,y)) =
+  if timeUntilMove - delta > 0
+  then MyWorld (timeUntilMove - delta) (x,y)
+  else step (delta - moveInterval) (MyWorld moveInterval (x+1, y))
+
+worldToPicture :: World -> Picture
+worldToPicture (MyWorld timeUntilMove (x,y)) =
+  Translate (fromIntegral x) (fromIntegral y) (Circle 60)
 
 main :: IO ()
-main = do
-  (runStateT loop 0)
-  return ()
-  
-loop :: StateT Int IO ()
-loop = do
-  input <- liftIO getLine
-  (out, exit) <- someFunc input 
-  liftIO $ putStrLn out
-  liftIO $ hFlush stdout
-  if exit
-    then return ()
-    else loop
-    
+main = play (InWindow "Nice Window" (200, 200) (10, 10)) white 30 initialWorld worldToPicture (\_ -> id) step
+
+
     
