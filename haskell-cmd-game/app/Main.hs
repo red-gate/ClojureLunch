a0767ec709813fdc7d83fdb72333d998eb9a464e
Toby Smyth <toby.smyth@red-gate.com>
2016-08-17 13:31:20 +0100

    Draw and move bats

--- haskell-cmd-game/app/Main.hs
+++ haskell-cmd-game/app/Main.hs
@@ -4,28 +4,68 @@ import Lib
 import Control.Monad.State.Strict
 import System.IO
 import Graphics.Gloss
+import Data.Monoid ((<>))
 
+data DirectionY = Up | Down
+  deriving Show
 
-data World = MyWorld { timeUntilMove  :: Float, position :: (Int, Int)  }
+data Bat = Bat (Maybe DirectionY) Float
+  deriving Show
+
+type Ball = (Float, Float)
+
+data World = MyWorld { ball :: Ball
+                     , p1 :: Bat
+                     , p2 :: Bat }
  deriving Show
 
-moveInterval :: Float
-moveInterval = 1
+initialBat :: Bat
+initialBat = Bat (Just Down) 0
 
-initialWorld = MyWorld { timeUntilMove = moveInterval, position = (20,20) }
+initialWorld = MyWorld { ball = (20,20), p1 = initialBat, p2 = initialBat}
 
 step :: Float -> World -> World
-step delta (MyWorld timeUntilMove (x,y)) =
-  if timeUntilMove - delta > 0
-  then MyWorld (timeUntilMove - delta) (x,y)
-  else step (delta - moveInterval) (MyWorld moveInterval (x+1, y))
+step d (MyWorld b p1 p2) =
+  MyWorld (stepBall d b)  (stepPlayer d p1) (stepPlayer d p2)
+
+stepBall :: Float -> Ball -> Ball
+stepBall delta (x,y) = (x + ballSpeed * delta, y) 
+
+stepPlayer d b@(Bat Nothing _) = b
+stepPlayer d (Bat c@(Just dir) p) = let f = case dir of
+                                          Up -> (+)
+                                          Down -> (-)
+                                        in Bat c (f p (d * ballSpeed))   
 
 worldToPicture :: World -> Picture
-worldToPicture (MyWorld timeUntilMove (x,y)) =
-  Translate (fromIntegral x) (fromIntegral y) (Circle 60)
+worldToPicture (MyWorld b p1 p2) = drawBall b <> (tP1 $ drawBat p1) <> (tP2 $ drawBat p2)
+  where
+    batTransDist = (gameWidth - batWidth) / 2
+    tP1 = translate (- batTransDist) 0
+    tP2 = translate batTransDist 0
+    
+drawBall :: Ball -> Picture  
+drawBall (x,y) = translate x y (circleSolid 60)
+
+drawBat :: Bat -> Picture
+drawBat (Bat _ pos) = translate 0 pos (rectangleSolid batWidth batHeight)
 
 main :: IO ()
-main = play (InWindow "Nice Window" (200, 200) (10, 10)) white 30 initialWorld worldToPicture (\_ -> id) step
+main = play (InWindow "Nice Window" (gameWidth, gameHeight) (10, 10)) white 30 initialWorld worldToPicture (\_ -> id) step
 
 
     
+ballSpeed :: Float
+ballSpeed = 10
+
+batWidth :: Num a => a
+batWidth = 10
+
+batHeight :: Num a => a
+batHeight = 100
+
+gameWidth :: Num a => a
+gameWidth = 500
+
+gameHeight :: Num a => a
+gameHeight = gameWidth
