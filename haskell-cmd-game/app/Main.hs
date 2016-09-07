e72c1c480cb8437ea7531a4e9f7cbb0e459c399d
Toby Smyth <toby.smyth@red-gate.com>
2016-09-07 13:09:14 +0100

    Bat collision

--- haskell-cmd-game/app/Main.hs
+++ haskell-cmd-game/app/Main.hs
@@ -8,23 +8,44 @@ import Graphics.Gloss
 import qualified Graphics.Gloss.Interface.Pure.Game as G
 import Data.Monoid ((<>))
 
+main :: IO ()
+main =
+  play
+    (InWindow "Nice Window" (gameWidth, gameHeight) (10, 10))
+    white
+    30
+    initialWorld
+    worldToPicture
+    input
+    step
 
-data DirectionX = Left | Right
+data DirectionX
+  = Left
+  | Right
   deriving (Show, Eq)
 
-data DirectionY = Up | Down
+data DirectionY
+  = Up
+  | Down
   deriving (Show, Eq)
 
-data Bat = Bat (Maybe DirectionY) Float
-  deriving Show
+data Bat =
+  Bat (Maybe DirectionY)
+      Float
+  deriving (Show)
 
-data Ball = Ball DirectionX DirectionY Float (Float, Float)
-  deriving Show
+data Ball =
+  Ball DirectionX
+       DirectionY
+       Float
+       (Float, Float)
+  deriving (Show)
 
-data World = MyWorld { ball :: Ball
-                     , p1 :: Bat
-                     , p2 :: Bat }
- deriving Show
+data World = MyWorld
+  { ball :: Ball
+  , p1 :: Bat
+  , p2 :: Bat
+  } deriving (Show)
 
 initialBat :: Bat
 initialBat = Bat Nothing 0
@@ -32,98 +53,171 @@ initialBat = Bat Nothing 0
 initialBall :: Ball
 initialBall = Ball Right Down 60 (-150, 20)
 
-initialWorld = MyWorld { ball = initialBall, p1 = initialBat, p2 = initialBat}
+initialWorld =
+  MyWorld
+  { ball = initialBall
+  , p1 = initialBat
+  , p2 = initialBat
+  }
+
+batRectangle :: Float -> Float -> Rectangle
+batRectangle x y = (x, y, batWidth, batHeight)
 
 step :: Float -> World -> World
-step d (MyWorld b p1 p2) =
-  MyWorld (stepBall d b)  (stepPlayer d p1) (stepPlayer d p2)
+step d (MyWorld b p1@(Bat _ p1y) p2@(Bat _ p2y)) =
+  let batTransDist = (gameWidth - batWidth) / 2
+  in MyWorld
+       (stepBall
+          d
+          [batRectangle (-batTransDist) p1y, batRectangle batTransDist p2y]
+          b)
+       (stepPlayer d p1)
+       (stepPlayer d p2)
 
-stepBall :: Float -> Ball -> Ball
-stepBall delta (Ball dirX dirY width (x,y)) =
-  let move = ballSpeed * delta in
-  let fX = case dirX of
-        Right -> (+)
-        Left -> (-)
-  in
-    let (dX, x') = edgeDetectX width $ (dirX, fX x move) in
-    let (dY, y') = edgeDetectY width $ stepBallY move dirY y in
-    Ball dX dY width (x', y') 
+stepBall :: Float -> [Rectangle] -> Ball -> Ball
+stepBall delta rectangles (Ball dirX dirY width (x, y)) =
+  let move = ballSpeed * delta
+      fX =
+        case dirX of
+          Right -> (+)
+          Left -> (-)
+      (dY, y') = edgeDetectY width $ stepBallY move dirY y
+      (dX, x') = edgeDetectX rectangles y width $ (dirX, fX x move)
+  in Ball dX dY width (x', y')
 
 stepBallY :: Float -> DirectionY -> Float -> (DirectionY, Float)
-stepBallY move dirY y = let fY = case dirY of
-                             Up -> (+)
-                             Down -> (-) in
-  (dirY, fY y move)
+stepBallY move dirY y =
+  let fY =
+        case dirY of
+          Up -> (+)
+          Down -> (-)
+  in (dirY, fY y move)
 
 edgeDetectY :: Float -> (DirectionY, Float) -> (DirectionY, Float)
-edgeDetectY width (dY, y) = if abs(y) + width >= (gameHeight/2)
-                     then (flipDirectionY dY, y)
-                     else (dY, y)
-                       
+edgeDetectY width (dY, y) =
+  if abs (y) + width >= (gameHeight / 2)
+    then (flipDirectionY dY, y)
+    else (dY, y)
+
 flipDirectionY :: DirectionY -> DirectionY
 flipDirectionY Up = Down
 flipDirectionY Down = Up
 
-edgeDetectX :: Float -> (DirectionX, Float) -> (DirectionX, Float)
-edgeDetectX width (dX, x) = if abs(x) + width >= (gameWidth/2)
-                     then (flipDirectionX dX, x)
-                     else (dX, x)
-                       
+edgeDetectX :: [Rectangle]
+            -> Float
+            -> Float
+            -> (DirectionX, Float)
+            -> (DirectionX, Float)
+edgeDetectX bats y width (dX, x) =
+  let hits = any (intersect (x, y, width)) bats
+  in if hits || abs (x) + width >= (gameWidth / 2)
+       then (flipDirectionX dX, x)
+       else (dX, x)
+
 flipDirectionX :: DirectionX -> DirectionX
 flipDirectionX Left = Right
 flipDirectionX Right = Left
 
+type Radius = Float
+
+type X = Float
+
+type Y = Float
+
+type Width = Float
+
+type Height = Float
+
+type Circle = (X, Y, Radius)
+
+type Rectangle = (X, Y, Width, Height)
+
+intersect :: Circle -> Rectangle -> Bool
+intersect (cX, cY, cR) (rX, rY, rW, rH)
+  | cdx > rW / 2 + cR = False
+  | cdy > rH / 2 + cR = False
+  | cdx <= rW / 2 = True
+  | cdy <= rH / 2 = True
+  | otherwise = cornerDistance <= cR ^ 2
+  where
+    cdx = abs (cX - rX)
+    cdy = abs (cY - rY)
+    cornerDistance = (cdx - rW / 2) ^ 2 + (cdy - rH / 2) ^ 2
+
 stepPlayer d b@(Bat Nothing _) = b
-stepPlayer d (Bat c@(Just dir) p) = let f = case dir of
-                                          Up -> (+)
-                                          Down -> (-)
-                                        in Bat c (f p (d * ballSpeed))   
+stepPlayer d (Bat c@(Just dir) p) =
+  let f =
+        case dir of
+          Up -> (+)
+          Down -> (-)
+  in Bat c (f p (d * ballSpeed))
 
 worldToPicture :: World -> Picture
-worldToPicture (MyWorld b p1 p2) = drawBall b <> (tP1 $ drawBat p1) <> (tP2 $ drawBat p2)
+worldToPicture (MyWorld b p1 p2) =
+  drawBall b <> (tP1 $ drawBat p1) <> (tP2 $ drawBat p2)
   where
     batTransDist = (gameWidth - batWidth) / 2
-    tP1 = translate (- batTransDist) 0
+    tP1 = translate (-batTransDist) 0
     tP2 = translate batTransDist 0
 
-drawBall :: Ball -> Picture  
-drawBall (Ball _ _ width (x,y)) = translate x y (circleSolid width)
+drawBall :: Ball -> Picture
+drawBall (Ball _ _ width (x, y)) = translate x y (circleSolid width)
 
 drawBat :: Bat -> Picture
 drawBat (Bat _ pos) = translate 0 pos (rectangleSolid batWidth batHeight)
 
 toggle :: G.KeyState -> DirectionY -> Bat -> Bat
-toggle G.Down dir (Bat _ p) = Bat (Just dir) p 
-toggle G.Up dir (Bat (Just oldDir) p) = let d = if (dir == oldDir) then Nothing else Just oldDir in
-  Bat d p 
+toggle G.Down dir (Bat _ p) = Bat (Just dir) p
+toggle G.Up dir (Bat (Just oldDir) p) =
+  let d =
+        if (dir == oldDir)
+          then Nothing
+          else Just oldDir
+  in Bat d p
 toggle G.Up _ b = b
 
 input :: G.Event -> World -> World
-input (G.EventKey (G.Char key) state _ _) w@(MyWorld _ p1 p2)=
+input (G.EventKey (G.Char key) state _ _) w@(MyWorld _ p1 p2) =
   case key of
-    'a' -> w {p1 = toggle state Up p1}
-    'z' -> w {p1 = toggle state Down p1}
-    'k' -> w {p2 = toggle state Up p2}
-    'm' -> w {p2 = toggle state Down p2}
+    'a' ->
+      w
+      { p1 = toggle state Up p1
+      }
+    'z' ->
+      w
+      { p1 = toggle state Down p1
+      }
+    'k' ->
+      w
+      { p2 = toggle state Up p2
+      }
+    'm' ->
+      w
+      { p2 = toggle state Down p2
+      }
     _ -> w
-input _ w = w 
+input _ w = w
 
-main :: IO ()
-main = play (InWindow "Nice Window" (gameWidth, gameHeight) (10, 10)) white 30 initialWorld worldToPicture input step
-
-
-    
 ballSpeed :: Float
 ballSpeed = 50
 
-batWidth :: Num a => a
+
+batWidth
+  :: Num a
+  => a
 batWidth = 10
 
-batHeight :: Num a => a
+batHeight
+  :: Num a
+  => a
 batHeight = 100
 
-gameWidth :: Num a => a
+gameWidth
+  :: Num a
+  => a
 gameWidth = 500
 
-gameHeight :: Num a => a
+gameHeight
+  :: Num a
+  => a
 gameHeight = gameWidth
