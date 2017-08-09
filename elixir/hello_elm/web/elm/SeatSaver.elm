2a62c18a56ecf032d0e2bfc0ea17705c32ce3d22
SimonHolness <simon.holness@red-gate.com>
2017-08-09 14:40:42 +0100

    strart working on messages in elm  (NOT WORKING)
    The presentation we were following is a couple of years out of date, elm
    has moved on from Signals to Messages, so we're now trying to cobble a
    way forward using those ... but it's not yet working

--- elixir/hello_elm/web/elm/SeatSaver.elm
+++ elixir/hello_elm/web/elm/SeatSaver.elm
@@ -2,6 +2,9 @@ module SeatSaver exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+
+import Task exposing (Task)
 
 type alias Seat =
   { seatNo : Int
@@ -10,8 +13,20 @@ type alias Seat =
 
 type alias Model = List Seat
 
-main = view init
+type Msg = Toggle Seat
 
+main : Program Never Model Msg
+main = program
+  { init = init
+  , update  = update
+  , view = view
+  , subscriptions = always <| Sub.none
+}
+
+port tasks : Signal (Task Never())
+port tasks = app.tasks
+
+init : (Model, Cmd Msg)
 init = [ { seatNo = 1, occupied = False }
   , { seatNo = 2, occupied = False }
   , { seatNo = 3, occupied = False }
@@ -24,8 +39,31 @@ init = [ { seatNo = 1, occupied = False }
   , { seatNo = 10, occupied = False }
   , { seatNo = 11, occupied = False }
   , { seatNo = 12, occupied = False }
-  ]
+  ] ! []
 
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    Toggle seatToToggle ->
+      let updateSeat seatFromModel =
+        if seatFromModel.seatNo == seatToToggle.seatNo
+        then {seatFromModel | occupied = not seatFromModel.occupied}
+        else seatFromModel
+      in
+        (List.map updateSeat model) ! []
+
+view : Model -> Html Msg
 view m = ul [class "seats"] (List.map seatItem m)
 
-seatItem seat = li [class "seat available"] [Html.text (toString seat.seatNo)]
+seatItem : Seat -> Html Msg
+seatItem seat =
+  let
+    seatText = if seat.occupied
+               then "occupied"
+               else toString seat.seatNo
+  in
+    li
+      [ class "seat available"
+      , onClick (Toggle seat)
+      ]
+      [ text seatText ]
--- elixir/hello_elm/web/static/vendor/seatsaver.js
+++ elixir/hello_elm/web/static/vendor/seatsaver.js
@@ -8145,31 +8145,131 @@ var _elm_lang$html$Html_Attributes$classList = function (list) {
 };
 var _elm_lang$html$Html_Attributes$style = _elm_lang$virtual_dom$VirtualDom$style;
 
-var _user$project$SeatSaver$seatItem = function (seat) {
+var _elm_lang$html$Html_Events$keyCode = A2(_elm_lang$core$Json_Decode$field, 'keyCode', _elm_lang$core$Json_Decode$int);
+var _elm_lang$html$Html_Events$targetChecked = A2(
+	_elm_lang$core$Json_Decode$at,
+	{
+		ctor: '::',
+		_0: 'target',
+		_1: {
+			ctor: '::',
+			_0: 'checked',
+			_1: {ctor: '[]'}
+		}
+	},
+	_elm_lang$core$Json_Decode$bool);
+var _elm_lang$html$Html_Events$targetValue = A2(
+	_elm_lang$core$Json_Decode$at,
+	{
+		ctor: '::',
+		_0: 'target',
+		_1: {
+			ctor: '::',
+			_0: 'value',
+			_1: {ctor: '[]'}
+		}
+	},
+	_elm_lang$core$Json_Decode$string);
+var _elm_lang$html$Html_Events$defaultOptions = _elm_lang$virtual_dom$VirtualDom$defaultOptions;
+var _elm_lang$html$Html_Events$onWithOptions = _elm_lang$virtual_dom$VirtualDom$onWithOptions;
+var _elm_lang$html$Html_Events$on = _elm_lang$virtual_dom$VirtualDom$on;
+var _elm_lang$html$Html_Events$onFocus = function (msg) {
 	return A2(
-		_elm_lang$html$Html$li,
-		{
-			ctor: '::',
-			_0: _elm_lang$html$Html_Attributes$class('seat available'),
-			_1: {ctor: '[]'}
-		},
-		{
-			ctor: '::',
-			_0: _elm_lang$html$Html$text(
-				_elm_lang$core$Basics$toString(seat.seatNo)),
-			_1: {ctor: '[]'}
-		});
+		_elm_lang$html$Html_Events$on,
+		'focus',
+		_elm_lang$core$Json_Decode$succeed(msg));
 };
-var _user$project$SeatSaver$view = function (m) {
+var _elm_lang$html$Html_Events$onBlur = function (msg) {
 	return A2(
-		_elm_lang$html$Html$ul,
-		{
-			ctor: '::',
-			_0: _elm_lang$html$Html_Attributes$class('seats'),
-			_1: {ctor: '[]'}
-		},
-		A2(_elm_lang$core$List$map, _user$project$SeatSaver$seatItem, m));
+		_elm_lang$html$Html_Events$on,
+		'blur',
+		_elm_lang$core$Json_Decode$succeed(msg));
 };
+var _elm_lang$html$Html_Events$onSubmitOptions = _elm_lang$core$Native_Utils.update(
+	_elm_lang$html$Html_Events$defaultOptions,
+	{preventDefault: true});
+var _elm_lang$html$Html_Events$onSubmit = function (msg) {
+	return A3(
+		_elm_lang$html$Html_Events$onWithOptions,
+		'submit',
+		_elm_lang$html$Html_Events$onSubmitOptions,
+		_elm_lang$core$Json_Decode$succeed(msg));
+};
+var _elm_lang$html$Html_Events$onCheck = function (tagger) {
+	return A2(
+		_elm_lang$html$Html_Events$on,
+		'change',
+		A2(_elm_lang$core$Json_Decode$map, tagger, _elm_lang$html$Html_Events$targetChecked));
+};
+var _elm_lang$html$Html_Events$onInput = function (tagger) {
+	return A2(
+		_elm_lang$html$Html_Events$on,
+		'input',
+		A2(_elm_lang$core$Json_Decode$map, tagger, _elm_lang$html$Html_Events$targetValue));
+};
+var _elm_lang$html$Html_Events$onMouseOut = function (msg) {
+	return A2(
+		_elm_lang$html$Html_Events$on,
+		'mouseout',
+		_elm_lang$core$Json_Decode$succeed(msg));
+};
+var _elm_lang$html$Html_Events$onMouseOver = function (msg) {
+	return A2(
+		_elm_lang$html$Html_Events$on,
+		'mouseover',
+		_elm_lang$core$Json_Decode$succeed(msg));
+};
+var _elm_lang$html$Html_Events$onMouseLeave = function (msg) {
+	return A2(
+		_elm_lang$html$Html_Events$on,
+		'mouseleave',
+		_elm_lang$core$Json_Decode$succeed(msg));
+};
+var _elm_lang$html$Html_Events$onMouseEnter = function (msg) {
+	return A2(
+		_elm_lang$html$Html_Events$on,
+		'mouseenter',
+		_elm_lang$core$Json_Decode$succeed(msg));
+};
+var _elm_lang$html$Html_Events$onMouseUp = function (msg) {
+	return A2(
+		_elm_lang$html$Html_Events$on,
+		'mouseup',
+		_elm_lang$core$Json_Decode$succeed(msg));
+};
+var _elm_lang$html$Html_Events$onMouseDown = function (msg) {
+	return A2(
+		_elm_lang$html$Html_Events$on,
+		'mousedown',
+		_elm_lang$core$Json_Decode$succeed(msg));
+};
+var _elm_lang$html$Html_Events$onDoubleClick = function (msg) {
+	return A2(
+		_elm_lang$html$Html_Events$on,
+		'dblclick',
+		_elm_lang$core$Json_Decode$succeed(msg));
+};
+var _elm_lang$html$Html_Events$onClick = function (msg) {
+	return A2(
+		_elm_lang$html$Html_Events$on,
+		'click',
+		_elm_lang$core$Json_Decode$succeed(msg));
+};
+var _elm_lang$html$Html_Events$Options = F2(
+	function (a, b) {
+		return {stopPropagation: a, preventDefault: b};
+	});
+
+var _user$project$SeatSaver$update = F2(
+	function (msg, model) {
+		var _p0 = msg;
+		var updateSeat = function (seatFromModel) {
+			return _elm_lang$core$Native_Utils.eq(seatFromModel.seatNo, _p0._0.seatNo) ? _elm_lang$core$Native_Utils.update(
+				seatFromModel,
+				{occupied: !seatFromModel.occupied}) : seatFromModel;
+		};
+		return A2(_elm_lang$core$List$map, updateSeat, model);
+	});
 var _user$project$SeatSaver$init = {
 	ctor: '::',
 	_0: {seatNo: 1, occupied: false},
@@ -8219,12 +8319,45 @@ var _user$project$SeatSaver$init = {
 		}
 	}
 };
-var _user$project$SeatSaver$main = _elm_lang$virtual_dom$Native_VirtualDom.staticProgram(
-	_user$project$SeatSaver$view(_user$project$SeatSaver$init));
 var _user$project$SeatSaver$Seat = F2(
 	function (a, b) {
 		return {seatNo: a, occupied: b};
 	});
+var _user$project$SeatSaver$Toggle = function (a) {
+	return {ctor: 'Toggle', _0: a};
+};
+var _user$project$SeatSaver$seatItem = function (seat) {
+	var seatText = seat.occupied ? 'occupied' : _elm_lang$core$Basics$toString(seat.seatNo);
+	return A2(
+		_elm_lang$html$Html$li,
+		{
+			ctor: '::',
+			_0: _elm_lang$html$Html_Attributes$class('seat available'),
+			_1: {
+				ctor: '::',
+				_0: _elm_lang$html$Html_Events$onClick(
+					_user$project$SeatSaver$Toggle(seat)),
+				_1: {ctor: '[]'}
+			}
+		},
+		{
+			ctor: '::',
+			_0: _elm_lang$html$Html$text(seatText),
+			_1: {ctor: '[]'}
+		});
+};
+var _user$project$SeatSaver$view = function (m) {
+	return A2(
+		_elm_lang$html$Html$ul,
+		{
+			ctor: '::',
+			_0: _elm_lang$html$Html_Attributes$class('seats'),
+			_1: {ctor: '[]'}
+		},
+		A2(_elm_lang$core$List$map, _user$project$SeatSaver$seatItem, m));
+};
+var _user$project$SeatSaver$main = _elm_lang$html$Html$beginnerProgram(
+	{model: _user$project$SeatSaver$init, view: _user$project$SeatSaver$view, update: _user$project$SeatSaver$update})();
 
 var Elm = {};
 Elm['SeatSaver'] = Elm['SeatSaver'] || {};
