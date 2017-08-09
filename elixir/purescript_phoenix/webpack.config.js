ba9a5be6ff9614deb6b3e75d1ac63e9cfcc7676c
Toby Smyth <toby.smyth@red-gate.com>
2017-08-09 10:50:20 +0100

    copy static

--- elixir/purescript_phoenix/webpack.config.js
+++ elixir/purescript_phoenix/webpack.config.js
@@ -1,7 +1,20 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
-  entry: "./web/static/js/app.js",
+  entry: ["./web/static/css/app.css", "./web/static/js/app.js"],
   output: {
-    path: __dirname + "/priv/static/js",
-    filename: "app.js"
-  }
+    path: __dirname + "/priv/static/",
+    filename: "js/app.js"
+  },
+ module: {
+    rules: [
+        { test: /\.(js|jsx)?$/, loader: 'babel-loader', exclude: /node_modules/,  options: { presets: [['es2015', { modules: false }]] }},
+        { test: /\.css$/, loader: ExtractTextPlugin.extract(
+            {
+                fallback: "style-loader",
+                use: "css-loader"
+            }
+        )}
+    ]},
+    plugins: [new ExtractTextPlugin({ filename: 'css/app.css' })]
 };
