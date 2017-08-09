7c4baaad78dcff5cc7c82cd4bf9bbc07ab1196d1
Toby Smyth <toby.smyth@red-gate.com>
2017-08-09 13:36:57 +0100

    purescript webpack

--- elixir/purescript_phoenix/webpack.config.js
+++ elixir/purescript_phoenix/webpack.config.js
@@ -2,9 +2,17 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
+  devtool: 'eval-source-map',
+
+  devServer: {
+    contentBase: '.',
+    port: 4008,
+    stats: 'errors-only'
+  },
   entry: ["./web/static/css/app.css", "./web/static/js/app.js"],
   output: {
     path: __dirname + "/priv/static/",
+    pathinfo: true,
     filename: "js/app.js"
   },
  module: {
@@ -15,7 +23,17 @@ module.exports = {
                 fallback: "style-loader",
                 use: "css-loader"
             }
-        )}
+        )},
+        {
+          test: /\.purs$/,
+          loader: 'purs-loader',
+          query: {
+            src: [ 'web/static/purescript/**/*.purs' ],
+            bundle: false,
+            pscIde: false,
+            pscPackage: true
+          }
+        }
     ]},
     plugins: [ new ExtractTextPlugin({ filename: 'css/app.css' })
              , new CopyWebpackPlugin([{ from: "./web/static/assets" }])]
