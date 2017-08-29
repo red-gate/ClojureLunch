3dcfac32d8ec2fdcde1cdef4a1f87761bacb953a
Toby Smyth <toby.smyth@red-gate.com>
2017-08-29 09:40:14 +0100

    Add channel

--- elixir/purescript_phoenix/web/channels/user_socket.ex
+++ elixir/purescript_phoenix/web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule PurescriptPhoenix.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", PurescriptPhoenix.RoomChannel
+  channel "room:*", PurescriptPhoenix.RoomChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
