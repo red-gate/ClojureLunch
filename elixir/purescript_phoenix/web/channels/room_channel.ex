74ec9818b6f7a63a2fae280377bbe96c27b15661
Toby Smyth <toby.smyth@red-gate.com>
2017-09-11 13:43:39 +0100

    msg properly

--- elixir/purescript_phoenix/web/channels/room_channel.ex
+++ elixir/purescript_phoenix/web/channels/room_channel.ex
@@ -4,6 +4,10 @@ use Phoenix.Channel
   def join("room:lobby", _message, socket) do
     {:ok, socket}
   end
+  def handle_in("new_message", payload, socket) do
+    broadcast! socket, "new_message", payload
+    {:noreply, socket}
+  end
 #  def join("room:" <> _private_room_id, _params, _socket) do
 #    {:error, %{reason: "unauthorized"}}
 #  end
--- elixir/purescript_phoenix/web/static/purescript/src/Main.purs
+++ elixir/purescript_phoenix/web/static/purescript/src/Main.purs
@@ -5,7 +5,7 @@ import Phoenix
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (log, CONSOLE)
 import DOM.HTML.Event.EventTypes (timeout)
-import Data.Foreign (Foreign, unsafeFromForeign)
+import Data.Foreign (Foreign, toForeign, unsafeFromForeign)
 import Prelude hiding (div,join)
 
 -- | Start and render the app
@@ -18,9 +18,12 @@ main = do
   sock <- newSocket "/socket" defaultSocketOptions
   connect sock
   chan <- channel sock "room:lobby" emptyJsObject
+  on chan "new_message" (\c e m -> log (unsafeFromForeign m))
   p <- join chan
   p2 <- receive p "ok" (\p d -> log "Joined lobby")
+  p3 <- push chan "new_message" $ toForeign {value: "I'm here"}
   log "Hello s"
+
 
 
 
