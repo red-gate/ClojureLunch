736109039fe5f89e8f35b1512a8b23d961fd0d32
SimonHolness <simon.holness@red-gate.com>
2017-09-20 17:04:08 +0100

    Message sending *nearly* works ...
    ... but we didn't quite have time to finish it :D ... so this is all in
    a broken state.
    You're welcome.

--- elixir/purescript_phoenix/web/static/purescript/src/Main.purs
+++ elixir/purescript_phoenix/web/static/purescript/src/Main.purs
@@ -3,17 +3,21 @@ module Main where
 import Phoenix
 
 import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (log, CONSOLE)
 import Control.Monad.Eff.Exception (EXCEPTION)
 import DOM.HTML.Event.EventTypes (timeout)
 import Data.Foreign (Foreign, toForeign, unsafeFromForeign)
+import Data.Maybe (Maybe(..))
+import Data.Traversable (traverse, traverse_)
 import Prelude hiding (div,join)
 import Pux (CoreEffects, EffModel, start)
 import Pux.DOM.Events (onChange, onClick, targetValue)
 import Pux.DOM.HTML (HTML)
 import Pux.Renderer.React (renderToDOM)
-import Signal.Channel (CHANNEL)
-import Text.Smolder.HTML (button, div, span, input)
+import Signal.Channel (CHANNEL, subscribe)
+import Signal.Channel as Sig
+import Text.Smolder.HTML (Html, button, div, input, span, ul, li)
 import Text.Smolder.HTML.Attributes (type', value)
 import Text.Smolder.Markup (text, (#!), (!))
 
@@ -21,18 +25,25 @@ import Text.Smolder.Markup (text, (#!), (!))
 foreign import emptyJsObject :: Foreign
 
 type AppEffects eff = (channel :: CHANNEL, exception :: EXCEPTION, phoenix :: PHOENIX, console :: CONSOLE | eff)
-type State = { msg :: String , chan :: Channel }
+type State = { msg :: String , chan :: Channel, msgs :: Array String }
 
 main :: forall eff. Eff (AppEffects eff)  Unit
 main = do
 
-  chan <- createChannel
+  sigChannel <- Sig.channel SendMessage  
+
+  sock <- newSocket "/socket" defaultSocketOptions
+  connect sock
+  chan <- channel sock "room:lobby" emptyJsObject
+  on chan "new_message" (\c e m -> Sig.send sigChannel (MessageReceived ((unsafeFromForeign m).value) ))
+  p <- join chan
+  p2 <- receive p "ok" (\p d -> log "Joined lobby")
 
   app <- start
-    { initialState: {msg: "Hello world", chan: chan }
+    { initialState: {msg: "Hello world", chan: chan, msgs: ["one", "two"] }
     , view
     , foldp
-    , inputs: []
+    , inputs: [ subscribe sigChannel ]
     }
 
   renderToDOM "#app" app.markup app.input
@@ -44,24 +55,16 @@ sendMessage chan msg =  do
   _ <- push chan "new_message" $ toForeign {value: msg}
   pure unit
   
-createChannel :: forall eff. Eff (AppEffects eff) Channel
-createChannel = do 
-  sock <- newSocket "/socket" defaultSocketOptions
-  connect sock
-  chan <- channel sock "room:lobby" emptyJsObject
-  on chan "new_message" (\c e m -> log (unsafeFromForeign m))
-  p <- join chan
-  p2 <- receive p "ok" (\p d -> log "Joined lobby")
-  pure chan
-
-
-
-data Event = TextUpdated String | SendMessage
+data Event = TextUpdated String | SendMessage | MessageReceived String
 
 -- | Return a new state (and effects) from each event
-foldp :: ∀ fx. Event -> State -> EffModel State Event fx
-foldp SendMessage s = { state: s, effects: [] }
-foldp (TextUpdated msg) s = { state: {msg: msg, chan: s.chan }, effects: [] }
+foldp :: ∀ fx. Event -> State -> EffModel State Event ( console :: CONSOLE, phoenix :: PHOENIX | fx )
+foldp SendMessage s = { state: s { msg = "" }  , 
+    effects: [ do 
+                _ <- liftEff $ sendMessage  s.chan s.msg
+                pure Nothing] }
+foldp (TextUpdated msg) s = { state: s { msg = msg }, effects: [] }
+foldp (MessageReceived msg) s = { state: s { msgs = (append s.msgs [msg])}, effects: [] }
 
 -- | Return markup from the state
 view :: State -> HTML Event
@@ -69,10 +72,6 @@ view state =
   div do
     button #! onClick (const SendMessage) $ text "Send Message"
     input ! type' "text" ! value state.msg #! onChange (\x -> TextUpdated (targetValue x) )
-
-
-
-
-
-
-
+    div do
+      ul do
+        traverse_ (\x -> li $ text x) state.msgs 
