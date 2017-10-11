ec1f3a21f008799bf2df22481ecc46811738b8b3
SimonHolness <simon.holness@red-gate.com>
2017-10-11 13:30:24 +0100

    Add a random 'token' to the socket
    This just shows that the sockets are unique per connected client

--- elixir/purescript_phoenix/web/channels/room_channel.ex
+++ elixir/purescript_phoenix/web/channels/room_channel.ex
@@ -4,8 +4,21 @@ use Phoenix.Channel
   def join("room:lobby", _message, socket) do
     {:ok, socket}
   end
+
+  def join("room:" <> _, _message, socket) do
+    {:ok, socket}
+  end
+
   def handle_in("new_message", payload, socket) do
-    broadcast! socket, "new_message", payload
+    
+    token = case Map.has_key?(socket.assigns, :token) do
+      true -> socket.assigns[:token]
+      false -> Enum.random(0..1024)
+    end
+    socket = assign(socket, :token, token)
+    load = Map.put(payload, :token, token)
+    IO.puts "adding token #{Kernel.inspect load} to socket"
+    broadcast! socket, "new_message" , load
     {:noreply, socket}
   end
   def handle_in("mouse_moved", payload, socket) do
