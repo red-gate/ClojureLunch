8f872eb7d9f42fb47eec9e2500299b599aeb1420
SimonHolness <simon.holness@red-gate.com>
2017-11-01 13:47:53 +0000

    send list update messages to channel
    Sorter doesn't know what kind of process we are, as its parent, so it
    just sends a message structure of {:update, list_update} which we cope
    with in our handle_info function. We just broadcast it to the socket.

--- elixir/purescript_phoenix/web/channels/sort_channel.ex
+++ elixir/purescript_phoenix/web/channels/sort_channel.ex
@@ -5,10 +5,15 @@ use Phoenix.Channel
     {:ok, socket}
   end
 
-
   def handle_in("sort_list", payload, socket) do
     # todo Some sort start code
     Sorter.start(payload["value"])
+    {:noreply, socket}
+  end
+
+  def handle_info({:update, list_update}, socket) do
+    # broadcast list update (complete with indices)
+    broadcast! socket, "list_update", %{value: Kernel.inspect(list_update)}
     {:noreply, socket}
   end
 
