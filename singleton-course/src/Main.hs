f9f198f2801b5b684aa18ffbff4025be9dde6241
Simon Holness <simon.holness@red-gate.com>
2019-08-07 13:13:42 +0100

    Added a whole bunch of comments
    While we discussed the code and tried to understand the Kind-level stuff

--- singleton-course/src/Main.hs
+++ singleton-course/src/Main.hs
@@ -1,6 +1,8 @@
 module Main where
 import Data.Text
 
+-- https://blog.jle.im/entry/introduction-to-singletons-1.html
+
 data Foo a = MkFoo Int
 
 data DoorState = Opened | Closed | Locked
@@ -9,10 +11,20 @@ data DoorState = Opened | Closed | Locked
 data Door (s :: DoorState) = UnsafeMkDoor { doorMaterial :: String, handleShape :: String }
   deriving (Show)
 
+  -- 'Opened etc are *type constructors* as opposed to the 
+  -- data constructors Opened etc on the DoorState datatype
 data SingDS :: DoorState -> * where
   SOpened :: SingDS 'Opened
   SClosed :: SingDS 'Closed
   SLocked :: SingDS 'Locked
+
+  -- this works, but note that Opened here is the same
+  -- *type constructor* as above ('Opened) NOT the 
+  -- data constructor
+data SingDSx :: DoorState -> * where
+    SOpenedx :: SingDSx Opened
+    SClosedx :: SingDSx Closed
+    SLockedx :: SingDSx Locked
 
 closeDoor :: Door 'Opened -> Door 'Closed
 closeDoor (UnsafeMkDoor m h) = UnsafeMkDoor m h
@@ -25,6 +37,20 @@ lockAnyDoor = \case
     SOpened -> lockDoor . closeDoor  -- in this branch, s is 'Opened
     SClosed -> lockDoor              -- in this branch, s is 'Closed
     SLocked -> id                    -- in this branch, s is 'Locked
+
+    -- note from the blog post ... 
+    -- In Haskell, a constraint SingDSI s => is essentially the same as passing in SingDS s explicitly. 
+    -- Either way, you are passing in a runtime witness that your function can use. 
+    -- You can think of SingDSI s => as passing it in implicitly, and SingDS s -> as passing it in explicitly.
+
+    -- So, it’s important to remember that lockAnyDoor and lockAnyDoor_ are the “same function”, 
+    -- with the same power. They are just written in different styles – 
+    -- lockAnyDoor is written in explicit style, and lockAnyDoor_ is written in implicit style.
+    
+lockAnyDoor_ :: SingDSI s => Door s -> Door 'Locked
+lockAnyDoor_ = lockAnyDoor singDS
+      
+  -- let b = lockAnyDoor_ (mkDoor SOpened "a" "b")
 
 unlockDoor :: Door 'Locked -> Door 'Closed
 unlockDoor (UnsafeMkDoor m h) = UnsafeMkDoor m h
@@ -55,9 +81,6 @@ instance SingDSI 'Closed where
 instance SingDSI 'Locked where
   singDS = SLocked
 
-lockAnyDoor_ :: SingDSI s => Door s -> Door 'Locked
-lockAnyDoor_ = lockAnyDoor singDS
-  
 doorStatus_ :: SingDSI s => Door s -> DoorState
 doorStatus_ = doorStatus singDS
 
