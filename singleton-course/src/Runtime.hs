fc9eceb703558b1e7a36c969763f24654a7c5191
Toby Smyth <toby.smyth@red-gate.com>
2019-08-28 13:18:02 +0100

    Fixup problems and continue with existential type

--- singleton-course/src/Runtime.hs
+++ singleton-course/src/Runtime.hs
@@ -1,22 +1,46 @@
-module Main where
-    import Data.Text
-    import Data.Singletons
-    import Data.Singletons.TH
+module Runtime where
+import Data.Text
+import Data.Singletons
+import Data.Singletons.TH
     
-    -- https://blog.jle.im/entry/introduction-to-singletons-2.html
-    
-   
-    data DoorState = Opened | Closed | Locked
-     deriving (Show, Eq)
+import Main
 
-    genSingletons [''DoorState]
+-- https://blog.jle.im/entry/introduction-to-singletons-2.html
 
-    data SomeDoor :: Type where
-        MkSomeDoor ::
-          { someDoorState    :: DoorState
-          , someDoorMaterial :: String
-          } -> SomeDoor
-    
-    main :: IO ()
-    main = do
-      putStrLn "hello world"
+-- stack ghc   -- ./src/Runtime.hs -ddump-splices -XTemplateHaskell
+
+-- getting these from Main now
+-- data DoorState = Opened | Closed | Locked
+--   deriving (Show, Eq)
+
+-- genSingletons [''DoorState]
+
+-- data Door :: DoorState -> * where
+--   UnsafeMkDoor :: { doorMaterial :: String } -> Door s
+
+data SomeDoor :: * where
+  MkSomeDoor :: Sing s -> Door s -> SomeDoor
+
+instance Show SomeDoor where
+  show (MkSomeDoor s d) = show d
+
+fromDoor :: Sing s -> Door s -> SomeDoor
+fromDoor = MkSomeDoor
+
+fromDoor_ :: SingI s => Door s -> SomeDoor
+fromDoor_ = fromDoor sing
+
+closeSomeOpenedDoor :: SomeDoor -> Maybe SomeDoor
+closeSomeOpenedDoor (MkSomeDoor s d) = case s of
+    SOpened -> Just . fromDoor_ $ closeDoor d
+    SClosed -> Nothing
+    SLocked -> Nothing
+
+lockAnySomeDoor :: SomeDoor -> SomeDoor
+lockAnySomeDoor (MkSomeDoor s d) = fromDoor_ $ lockAnyDoor s d
+
+mkSomeDoor :: DoorState -> String -> String -> SomeDoor
+mkSomeDoor ds s = case ds of 
+    Opened -> fromDoor_ . (mkDoor SOpened s)
+    Closed -> fromDoor_ . (mkDoor SClosed s)
+    Locked -> fromDoor_ . (mkDoor SLocked s)
