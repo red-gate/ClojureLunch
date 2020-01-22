ec8b664ae7b270766b2369dde417ce75a475bc0e
Clive Tong <clive.tong@red-gate.com>
2020-01-22 08:47:47 +0000

    More notes

--- simple-typechecker/Program.fs
+++ simple-typechecker/Program.fs
@@ -13,13 +13,19 @@ let a =
   let id x = x
   id 2
 
+// We'll do this the functinal way (though this makes some things a lot harder)
+//  - typically you'd use mutable slots to represent the substituions that we'll end up doing by rewrites
+
 // Define the type representing types
 
 // And a means to get new type variables
 
-// We'll also need type environments and type schemas
+// We'll also need type environments and type schemes
 
 // The basic technique requires unification and substitution so let's write those on some simple examples
+//  - what's unification (and the most general unifier)
+//  - how do we apply a subsitution to a type/type scheme/type environment
+//  - and compose substituions
 
 // instantiate to get fresh variables when we look something up
 // We can now put the identity in the type environment and look it up
@@ -28,6 +34,8 @@ let a =
 // We can now generalise the identity function
 
 // Now we just put it all together
+
+// The typecheck worker function will return a type and a substitution
 
 // Let's try
 
