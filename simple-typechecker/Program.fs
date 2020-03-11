6a2eac724a568fd4a6386d7c21ac1553afa97046
Clive Tong <email@example.com>
2020-03-11 18:14:52 +0000

    Add more translations

--- simple-typechecker/Program.fs
+++ simple-typechecker/Program.fs
@@ -374,7 +374,11 @@ let rec translate expr =
   match expr with
   | Application (exp1, exp2) -> App(translate exp1, translate exp2)
   | Let (var, exp1, exp2) -> Let(var.Name, translate exp1, translate exp2)
+  | LetRecursive ([var, exp1], exp2) -> Let(var.Name, translate exp1, translate exp2)
   | Lambda (x, exp1) -> Abs(x.Name, translate exp1)
   | Var var -> Exp.Var(var.Name)
   | _ -> failwithf "Can't handle %A " expr
 
+translate <@@ let rec bottom = (fun x -> bottom x) : int -> int in bottom @@>
+
+translate <@@ let id = fun x -> x : int -> int in id @@>
